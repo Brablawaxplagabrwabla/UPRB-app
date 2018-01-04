4dad1ca8b0a9d4fb203ad8dc3c4ce5ccb85fd9e0
brablawaxplagabrwabla <gtroncone@correo.unimet.edu.ve>
2018-01-04 15:24:57 -0400

    Cambios a la base de datos

---
+++ src/componentes/ListaEstatus.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import Estatus from './Estatus';
+
+class ListaEstatus extends Component {
+	render() {
+		return (
+			<Text>ListaEstatus funciona</Text>
+		);
+	}
+}
+
+export default ListaEstatus;
